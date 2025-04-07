3dadd563ca10af86eccc818f698a640d6bf16878
ritviksaxena <78605818+ritviksaxena4@users.noreply.github.com>
2025-04-07 16:53:09 +0530

    Updated output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-##output "eks_cluster_name" {
-  ##value = aws_eks_cluster.eks_cluster.name
-##}
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
 
-##output "ansible_server_ip" {
-## value = aws_instance.ansible_server.public_ip
-##}
+output "ansible_server_ip" {
+  value = aws_instance.ansible_server.public_ip
+}
